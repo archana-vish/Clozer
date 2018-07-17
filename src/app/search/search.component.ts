c9ceb98b96f13064ad1e47a79ca369acf9f7953a
Archana Visveswaran <A.Visveswaran@greenwich.ac.uk>
2018-07-17 13:33:16 +0100

    further changes to search screen

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  priceRange: Array<any> = new Array(20);
+
+
   constructor() { }
 
   ngOnInit() {
+    for (var j = 0, i = 50000; j < 20; i += 10000) {
+      this.priceRange[j] = i;
+      j++;
+    }
   }
+
+
+
 
 }
